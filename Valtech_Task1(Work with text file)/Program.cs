731d10b7ea22546b260defac0db129b9a6198925
Vladyslav Levchenko <vladyslav.levchenko@valtech.com>
2021-11-15 15:15:16 +0200

    Harassment fixing

--- Valtech_Task1(Work with text file)/Program.cs
+++ Valtech_Task1(Work with text file)/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Valtech_Task1_Work_with_text_file_
 {
@@ -8,16 +7,23 @@ namespace Valtech_Task1_Work_with_text_file_
     {
         static void Main(string[] args)
         {
-            string word = " ";
-            WorkWithFile file = new WorkWithFile(@"C:\Users\vladyslav.levchenko\Desktop\Text.txt"); // <--- change file path here
-            file.ReadFile();
+            var word = string.Empty;
+            WorkWithFile file = new WorkWithFile();
+            while(file.IsCorrectPath == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Enter path to the file: ");
+                file.Path = Console.ReadLine();
+                file.ReadFile();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
             file.GenerateDictionaryOfAllWords();
             Console.WriteLine();
             file.Statistic();
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("Write some string for searching in the text or enter 'xxx' for exit: ");
+                Console.Write("Write some word for searching in the text or enter 'xxx' for exit: ");
                 word = Console.ReadLine();
                 Console.ForegroundColor = ConsoleColor.White;
                 if (word == "xxx")
@@ -25,11 +31,9 @@ namespace Valtech_Task1_Work_with_text_file_
                     Console.WriteLine("Bye-bye");
                     break;
                 }
-
                 if (word != null)
                 {
-                    file.SearchWord(word.Trim());
-                    //file.WriteToFile(file.Indexes,word);
+                    file.SearchWords(word.Trim());
                 }
                 file.Indexes = new List<int>(file.Content.Length);
                 file.AllWords = new Dictionary<int, List<string>>(file.Content.Length);
--- Valtech_Task1(Work with text file)/WorkWithFile.cs
+++ Valtech_Task1(Work with text file)/WorkWithFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,132 +9,131 @@ namespace Valtech_Task1_Work_with_text_file_
 {
     public class WorkWithFile
     {
-        /// <summary>
-        /// Path to the file
-        /// </summary>
         public string Path { get; set; }
-
-        /// <summary>
-        /// General content of the file
-        /// </summary>
         public string Content { get; set; }
-
-        /// <summary>
-        /// Copy of the general content
-        /// </summary>
         public string CopyContent { get; set; }
-
-        /// <summary>
-        /// List of the start indexes of each searching words
-        /// </summary>
         public List<int> Indexes { get; set; }
-
-        /// <summary>
-        /// Dictionary for all words in the text
-        /// </summary>
         public Dictionary<int, List<string>> AllWords { get; set; }
-
-        /// <summary>
-        /// Initialize path
-        /// </summary>
-        /// <param name="path"></param>
-        public WorkWithFile(string path)
+        public bool IsCorrectPath { get; set; }
+        public WorkWithFile()
         {
-            Path = path ?? throw new ArgumentNullException("Path can't be null.");
             AllWords = new Dictionary<int, List<string>>(1000);
+            IsCorrectPath = false;
         }
-
-        /// <summary>
-        /// Reading from file and initialize content
-        /// </summary>
-        public async void ReadFile()
+        public void ReadFile()
         {
             try
             {
                 using StreamReader sr = new StreamReader(Path);
-                Content = await sr.ReadToEndAsync();
+                Content = sr.ReadToEnd();
+                IsCorrectPath = true;
                 Indexes = new List<int>(Content.Length);
                 CopyContent = Content;
                 CopyContent = CopyContent.ToLower();
             }
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(ex.Message);
+                IsCorrectPath = false;
             }
-        }
-        /// <summary>
-        /// Write starts indexes to the file which we try to find
-        /// </summary>
-        /// <param name="indexes"></param>
-        /// <param name="word"></param>
-        public void WriteToFile(List<int> indexes,string word)
-        {
-            indexes.WriteToFileList(word);
         }
         /// <summary>
         /// Find the word in the text and remember start position of each word
         /// </summary>
         /// <param name="word"></param>
-        public void SearchWord(string word)
+        public void SearchWords(string word)
             {
-       
-            StringBuilder tmp = new StringBuilder(25);
-            word = word.ToLower();
-            for (int i = 0; i < CopyContent.Length; ++i)
-            {
-                if (CopyContent[i].Equals(' ') ||
-                    CopyContent[i].Equals('\n') ||
-                    CopyContent[i].Equals('.') ||
-                    CopyContent[i].Equals(',') ||
-                    CopyContent[i].Equals('!')||
-                    CopyContent[i].Equals('?')||
-                    CopyContent[i].Equals('(')||
-                    CopyContent[i].Equals(')')||
-                    CopyContent[i].Equals('[')||
-                    CopyContent[i].Equals(']')||
-                    CopyContent[i].Equals('\"')||
-                    CopyContent[i].Equals('\''))
+                StringBuilder tmp = new StringBuilder(25);
+                word = word.ToLower();
+                for (int i = 0; i < CopyContent.Length; ++i)
                 {
-                    if (tmp.Equals(word))
+                    if ((Char.IsControl(CopyContent[i]) || Char.IsPunctuation(CopyContent[i]) || Char.IsWhiteSpace(CopyContent[i]) || Char.IsDigit(CopyContent[i])))
                     {
-                        int start = i - tmp.Length;
-                        Indexes.Add(start);
+                        if (tmp.Equals(word))
+                        { 
+                            int start = i - tmp.Length;
+                            Indexes.Add(start);
+                        }
+                        tmp.Clear();
                     }
+                    else
+                    {
+                        tmp.Append(CopyContent[i]);
+                    }
+                }
+
+                if (Indexes.Count != 0)
+                {
+                    Show(word.Length);
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine();
+                    Console.Write($"All indexes of \'{word}\' ({Indexes.Count}) word: ");
+                    foreach (var ind in Indexes)
+                        Console.Write($"{ind} ");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"In this text file word \'{word}\' is not found!");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+               
+                Console.WriteLine();
+        }
+        /// <summary>
+        /// Generate a dictionary of all words with printing all statistics about each word in the text file with word's position and line
+        /// </summary>
+        public void GenerateDictionaryOfAllWords()
+        {
+            var stringLine = 1;
+            var position = 1;
+            StringBuilder tmp = new StringBuilder(25);
+            for (int i = 0; i < CopyContent?.Length; ++i)
+            {
+                if (Char.IsControl(CopyContent[i]) || Char.IsPunctuation(CopyContent[i]) || Char.IsWhiteSpace(CopyContent[i]) || Char.IsDigit(CopyContent[i]))
+                {
+                    if (CopyContent[i].Equals('\n'))
+                    {
+                        ++stringLine;
+                        position = 1;
+                    }
+                    var startIndex = i - tmp.Length;
+                    if(tmp.ToString() != "")
+                        AllWords.Add(startIndex,new List<string>(){tmp.ToString(),stringLine.ToString(),(position - tmp.Length).ToString()});
                     tmp.Clear();
                 }
                 else
                 {
                     tmp.Append(CopyContent[i]);
                 }
+                ++position;
             }
-
-            if (Indexes.Count != 0)
+            PrintFullDictionaryWithStatistic();
+        }
+        public void Statistic()
+        {
+            var statistics = AllWords.GroupBy(x => x.Value.ElementAt(0));
+            var enumerable = statistics as IGrouping<string, KeyValuePair<int, List<string>>>[] ?? statistics.ToArray();
+            foreach (var item in enumerable.OrderByDescending(x => x.Count()))
             {
-                Show(word.Length);
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine();
-                Console.Write($"All indexes of \'{word}\' ({Indexes.Count}) word: ");
-                foreach (var ind in Indexes)
-                    Console.Write($"{ind} ");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
+                double percent = Math.Round(Convert.ToDouble(item.Count()) * 100.0 / Convert.ToDouble(statistics.Count()), 2);
+                Console.WriteLine($"Amount: {item.Count()}\t Percent: {percent}%\t Word: \'{item.Key}\'");
             }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"In this text file word \'{word}\' is not found!");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-            }
-               
             Console.WriteLine();
         }
-        /// <summary>
-        /// Display all words with another color if this word we are searching
-        /// </summary>
-        /// <param name="size"></param>
+        private void PrintFullDictionaryWithStatistic()
+        {
+            foreach (var dic in AllWords)
+            {
+                Console.WriteLine($"Position: {dic.Value[2]}\t Index: {dic.Key}\t Line: {dic.Value[1]}\t Word: \'{dic.Value[0]}\'");
+            }
+        }
         private void Show(int size)
         {
-            int index = 0;
+            var index = 0;
             for (int i = 0; i < Content.Length;)
             {
                 if (i == Indexes[index])
@@ -156,72 +156,6 @@ namespace Valtech_Task1_Work_with_text_file_
                     ++i;
                 }
             }
-        }
-        /// <summary>
-        /// Generate a dictionary of all words with printing all statistics about each word in the text file with word's position and line
-        /// </summary>
-        public void GenerateDictionaryOfAllWords()
-        {
-            int stringLine = 1;
-            int position = 1;
-            StringBuilder tmp = new StringBuilder(25);
-            for (int i = 0; i < CopyContent?.Length; ++i)
-            {
-                if (CopyContent[i].Equals(' ') ||
-                    CopyContent[i].Equals('\n') ||
-                    CopyContent[i].Equals('.') ||
-                    CopyContent[i].Equals(',') ||
-                    CopyContent[i].Equals('!') ||
-                    CopyContent[i].Equals('?') ||
-                    CopyContent[i].Equals('(') ||
-                    CopyContent[i].Equals(')') ||
-                    CopyContent[i].Equals('[') ||
-                    CopyContent[i].Equals(']') ||
-                    CopyContent[i].Equals('\"') ||
-                    CopyContent[i].Equals('\r')||
-                    CopyContent[i].Equals(':'))
-                {
-                    if (CopyContent[i].Equals('\n'))
-                    {
-                        ++stringLine;
-                        position = 1;
-                    }
-                    var startIndex = i - tmp.Length;
-                    if(tmp.ToString() != "")
-                        AllWords.Add(startIndex,new List<string>(){tmp.ToString(),stringLine.ToString(),(position - tmp.Length).ToString()});
-                    tmp.Clear();
-                }
-                else
-                {
-                    tmp.Append(CopyContent[i]);
-                }
-                ++position;
-            }
-            PrintFullDictionaryWithStatistic();
-        }
-        /// <summary>
-        /// Printing information about each word
-        /// </summary>
-        private void PrintFullDictionaryWithStatistic()
-        {
-            foreach (var dic in AllWords)
-            {
-                Console.WriteLine($"Position: {dic.Value[2]}\t Index: {dic.Key}\t Line: {dic.Value[1]}\t Word: \'{dic.Value[0]}\'");
-            }
-        }
-        /// <summary>
-        /// Print all statistics about each word
-        /// </summary>
-        public void Statistic()
-        {
-            var statistics = AllWords.GroupBy(x => x.Value.ElementAt(0));
-            var enumerable = statistics as IGrouping<string, KeyValuePair<int, List<string>>>[] ?? statistics.ToArray();
-            foreach (var item in enumerable.OrderByDescending(x => x.Count()))
-            {
-                double percent = Math.Round(Convert.ToDouble(item.Count()) * 100.0 / Convert.ToDouble(statistics.Count()),2);
-                Console.WriteLine($"Amount: {item.Count()}\t Percent: {percent}%\t Word: \'{item.Key}\'");
-            }
-            Console.WriteLine();
         }
     }
 }
